20f1cbc271d168a7295ea0f7067fc6ab3ac47fd7
rohitahirwal <ahirwar0206@gmail.com>
2025-11-20 18:30:28 +0530

    modified server.js

--- server.js
+++ server.js
@@ -52,33 +52,3 @@ const port = process.env.port || 8080
 server.listen(port,()=>{
        console.log(`server is running hello rohit ${port}`)
 })
-
-// ************************************************************
-
-
-const express = require('express')
-const cors = require('cors')
-const fileuploader = require('express-fileupload')
-const Cloudinary = require('cloudinary').v2
-
-require('dotenv').config()
-
-const server1 = express()
-server.use(express.json())
-server.use(cors({
-      origin : 'http://localhost:3000' ,
-      credentials : true
-}))
-server.use(fileuploader())
-
-Cloudinary.config({
-     cloud_name : CLOUD_NAME,
-     api_key : CLOUD_API_KEY,
-     api_secret : CLOUD_API_SECRET
-})
-
-
-const port1 = process.env.PORT || 8089
-server1.listen(port1,()=>{
-       console.log(`http://localhost:${port}`)
-})
